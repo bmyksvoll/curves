428b2e67475a8bd1153c9e43218cc890d59219c6
Bjarte Myksvoll <bjarte.myksvoll@live.no>
2024-06-12 13:37:29 +0200

    Solved it! Have to include lower boundary of integral (not zero).

--- curves/volatility.py
+++ curves/volatility.py
@@ -9,10 +9,10 @@ def var_bs_integral(t, a= 0.9, b = 0.6, c= 0.1):
 
 
 def var_bs_F1(t, a= 0.9, b = 0.6):
-    return -a**2/(b + t)
+    return a**2/(b + t)
     
 def var_bs_F2(t, a= 0.9, b = 0.6, c = 0.1):
-    return 2*a*c*log(b + t)
+    return -2*a*c*log(b + t)
 
 def var_bs_F3(t, c = 0.1):
     return c**2*t
@@ -26,12 +26,15 @@ a= 0.9
 b = 0.6
 c= 0.1
     
-    
+sigma_bs(1) 
+
+
 
 #Test Function equivalence
 #print("Test Function equivalence:")
-print(var_bs_integral(t, a,b,c)/1)
-print(var_bs_F1(t, a,b)+var_bs_F2(t, a,b,c)+var_bs_F3(t, c))
+print(var_bs_integral(t, a,b,c)-var_bs_integral(0, a,b,c))
+
+#print(var_bs_F1(t, a,b)+var_bs_F2(t, a,b,c)+var_bs_F3(t, c))
 
 
 
@@ -41,9 +44,9 @@ print(var_bs_F1(t, a,b)+var_bs_F2(t, a,b,c)+var_bs_F3(t, c))
 # 
 # print("Test Integral equivalence:")
 
-print(var_bs_F1(t, a,b))
-print(var_bs_F2(t, a,b,c))
-print(var_bs_F3(t, c))
+#print(var_bs_F1(t, a,b))
+#print(var_bs_F2(t, a,b,c))
+#print(var_bs_F3(t, c))
 
 
 
